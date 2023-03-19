ba6e7fd834ea2edc77ff72294e21f34335540f01
sorhan <sorhan3.14@gmail.com>
2023-03-19 12:28:18 +0300

    create createpost endpoint

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
-def root():
+def get_user():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your post"}
+
+@app.post("/createpost")
+def create_post(payLoad: dict = Body(...)):
+    return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
