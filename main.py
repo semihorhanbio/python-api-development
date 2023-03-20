9794f51fab0feb518339f5f8273fbfa0244673e2
sorhan <sorhan3.14@gmail.com>
2023-03-20 08:54:52 +0300

    create Post model

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def get_user():
@@ -12,5 +20,5 @@ def get_posts():
     return {"data": "This is your post"}
 
 @app.post("/createpost")
-def create_post(payLoad: dict = Body(...)):
-    return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
+def create_post(post: Post):
+    return {"data": post}
