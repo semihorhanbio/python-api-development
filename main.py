ef7055ba9a82a4f202eec182e920ef967eb9ca21
sorhan <sorhan3.14@gmail.com>
2023-03-20 11:27:44 +0300

    change status code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
@@ -23,8 +22,8 @@ def get_user():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
-def create_post(post: Post):
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post,):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 100000)
     my_posts.append(post_dict)
@@ -34,6 +33,7 @@ def create_post(post: Post):
 @app.get("/posts/{id}")
 def get_post(id: int):
     post = list(filter(lambda p: p['id'] == id, my_posts))
-    if post:
-        return {"data": post[0]}
-    return {"data": "null"}
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'post with id {id} was not found')
+    return {"data": post[0]}
