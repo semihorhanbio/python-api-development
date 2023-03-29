46d769c7c660021fa11d65192a88ae72dcee9095
sorhan <sorhan3.14@gmail.com>
2023-03-29 12:06:44 +0300

    change createposts endpoint

--- app/main.py
+++ app/main.py
@@ -46,13 +46,11 @@ def get_posts(db: Session = Depends(get_db)):
     return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post,):
-    cursor.execute("""
-        INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) 
-        RETURNING *""", (post.title, post.content, post.published))
-
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_post(post: Post, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     
     return {"data": new_post}
 
