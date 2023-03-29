2a21a251eeba4e077686b2f70d721a3415a88cfa
sorhan <sorhan3.14@gmail.com>
2023-03-29 12:23:34 +0300

    update delete post endpoint

--- app/main.py
+++ app/main.py
@@ -63,16 +63,17 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return {"data": post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (str(id)))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
 
-    if not deleted_post:
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f'post with id {id} was not found')
     
-    return {"data": deleted_post}
+    post.delete(synchronize_session=False)
+    db.commit()
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
