e80564026935484045ed4e3dc86fd5c4751108cb
sorhan <sorhan3.14@gmail.com>
2023-04-05 12:26:28 +0300

    create getuser with id endpoint

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from time import sleep
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, utils
 from .database import SessionLocal, engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -79,9 +79,23 @@ def update_post(id: int, updated_post: schemas.Post, db: Session = Depends(get_d
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # hash the password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
     return new_user
+
+@app.get('/users/{id}', response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'User with id {id} does not found')
+    return user
