d6eb597e85d87f95a8a615bdd1527509a5eced76
sorhan <sorhan3.14@gmail.com>
2023-04-05 11:32:21 +0300

    create usercreate pydantic model

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class Post(BaseModel):
     title: str
@@ -15,3 +15,7 @@ class PostResponse(BaseModel):
 
     class Config:
         orm_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
