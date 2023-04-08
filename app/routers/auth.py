6daf3e14bd99df9c330474d0148a4b07709d1a74
sorhan <sorhan3.14@gmail.com>
2023-04-08 11:43:41 +0300

    change status code login

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -10,10 +10,10 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credetials."))
+        raise(HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credetials."))
     
     if not utils.verify(user_credentials.password, user.password):
-         raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credetials."))
+         raise(HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credetials."))
     
     # create a token
     access_token = oauth2.create_access_token(data={"user_id": user.id})
