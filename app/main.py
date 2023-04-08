319c5da2760a67bb62c3de9c1d09b3ec27427888
sorhan <sorhan3.14@gmail.com>
2023-04-08 10:29:11 +0300

    create login process

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import SessionLocal, engine, get_db
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -27,6 +27,7 @@ while True:
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def get_user():
